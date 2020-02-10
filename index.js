0a10f3330fb8895880bead7e07e58e841ac0d46c
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-09 23:14:42 -0500

    setting up mongoose and db

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
