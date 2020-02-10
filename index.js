ee09f776a1caae5a51d3aa4813d4b83db5847ece
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-09 23:31:47 -0500

    Added url schema

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(express.json({ extended: false }));
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
