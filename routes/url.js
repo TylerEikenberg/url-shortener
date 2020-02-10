c85cd6871f28be3912522971799dfab2f0b87d0e
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-09 23:54:10 -0500

    url complete, fixed json baseUr name

--- routes/url.js
+++ routes/url.js
@@ -8,16 +8,44 @@ const Url = require('../models/url');
 
 // @route   POST /api/urls/shorten
 // @desc    Create short url
-router.post('/shorten', (req, res) => {
+router.post('/shorten', async (req, res) => {
   const { longUrl } = req.body;
   const baseUrl = config.get('baseUrl');
 
+  // check base url
   if (!validUrl.isUri(baseUrl)) {
     return res.status(401).json('Invalid base url');
   }
 
   // create url code
   const urlCode = shortid.generate();
+
+  // check longUrl
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + '/' + urlCode;
+
+        url = new Url({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date()
+        });
+        await url.save();
+
+        res.json(url);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json('Server error');
+    }
+  } else {
+    res.status(401).json('Invalid long url');
+  }
 });
 
 module.exports = router;
